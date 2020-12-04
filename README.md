04602ba59d7ab6855b7b59038be2649ba411a1b7
ramya6492 <71939280+ramya6492@users.noreply.github.com>
2020-12-04 16:00:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C37
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ js/Player.js
@@ -0,0 +1,33 @@
+class Player {
+  constructor(){
+    
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
